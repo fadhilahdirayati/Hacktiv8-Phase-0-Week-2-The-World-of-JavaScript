069068676891cdd62b707ff7a050fecd0be5c2b2
fadhilahdirayati <fadhilahdirayati@gmail.com>
2018-04-02 13:19:46 +0700

    Update exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -2,58 +2,18 @@ var tanggal = 20; // assign nilai variabel tanggal disini! (dengan angka antara 
 var bulan = 6;// assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1996; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
 
-switch (true){
-  case tanggal < 30 && tanggal > 0:
-    console.log(tanggal)
-    break
-  default:
-    console.log("salah")
-}
-
-switch (bulan) {
-  case 1:
-    console.log("januari")
-    break
-  case 2:
-    console.log("februari")
-    break
-  case 3:
-    console.log("maret")
-    break
-  case 4:
-    console.log("april")
-    break
-  case 5:
-    console.log("mei")
-    break
-  case 6:
-    console.log("juni")
-    break
-  case 7:
-    console.log("juli")
-    break
-  case 8:
-    console.log("agustus")
-    break
-  case 9:
-    console.log("september")
-    break
-  case 10:
-    console.log("oktober")
-    break
-  case 11:
-    console.log("November")
-    break
-  case 12:
-    console.log("Desember")
-  default:
-    console.log("salah")
-}
-
-switch (true){
-  case tahun < 2200 && tahun > 1900:
-    console.log(tahun)
-    break
-  default:
-    console.log("salah")
+switch(bulan) {
+  case 1: { console.log(tanggal + ' Januari ' + tahun); break; }
+  case 2: { console.log(tanggal + ' Februari ' + tahun); break; }
+  case 3: { console.log(tanggal + ' Maret ' + tahun); break; }
+  case 4: { console.log(tanggal + ' April ' + tahun); break; }
+  case 5: { console.log(tanggal + ' Mei ' + tahun); break; }
+  case 6: { console.log(tanggal + ' Juni ' + tahun); break; }  
+  case 7: { console.log(tanggal + ' Juli ' + tahun); break; }
+  case 8: { console.log(tanggal + ' Agustus ' + tahun); break; }
+  case 9: { console.log(tanggal + ' September ' + tahun); break; }
+  case 10: { console.log(tanggal + ' Oktober ' + tahun); break; }
+  case 11: { console.log(tanggal + ' November ' + tahun); break; }
+  case 12: { console.log(tanggal + ' Desember ' + tahun); break; }
+  default : { console.log('Bulan tidak tersedia'); }
 }
