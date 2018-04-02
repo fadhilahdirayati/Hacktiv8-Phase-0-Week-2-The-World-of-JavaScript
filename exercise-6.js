ca7d18d95af84578105009ee2fc4ad0fde670b7a
fadhilahdirayati <fadhilahdirayati@gmail.com>
2018-04-02 13:16:16 +0700

    Update exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -33,64 +33,66 @@ while (x >=1){
 /* 3 */
 
 
+// Perulangan Ganjil Genap
+
 for (var i = 1; i <= 100 ; i ++) {
-
-
- if(i % 2 === 0){
-
-   console.log ( i + ' - GENAP')
-
- } else if (i % 2 !== 0 ){
-
-   console.log (i + ' - GANJIL')
- }
-
-
-
+  
+  
+  if(i % 2 === 0){
+    
+    console.log ( i + ' - GENAP')
+    
+  } else if (i % 2 !== 0 ){
+    
+    console.log (i + ' - GANJIL')
+  }
+  
+ 
+  
 }
 
-//////////////////////////////////////////
+//kelipatan 3 dengan pertambahan 2
 for (var i = 1; i <=100; i+= 2){
-
-
-if (i % 3 === 0){
-
-  console.log (i + ' 3 KELIPATAN  ' + 3)
-
-} else {
-
-  console.log(i + " - ")
+  
+ 
+ if (i % 3 === 0){
+   
+   console.log (i + ' 3 KELIPATAN ' + 3)
+   
+ } else {
+   
+   console.log(i + " - ")
+ }
+ 
 }
 
-}
 
-/////////////////////////////////////////
-
-
+//kelipatan 6 dengan pertambahan 5
 
 for (var i = 1; i <=100; i+= 5){
-
-  if (i % 6 === 0){
-
-  console.log (i + '6 KELIPATAN ' + 6)
-
-} else {
-
-  console.log(i + " - ")
+  
+   if (i % 6 === 0){
+   
+   console.log (i + ' 6 KELIPATAN ' + 6)
+   
+ } else {
+   
+   console.log(i + " - ")
+ }
+ 
 }
 
-}
-
-///////////////////////////////////////
-
+//kelipatan 10 dengan pertambahan 9,
 
 for (var i = 1; i <=100; i+= 9){
-
-if (i % 10 === 0){
-
-  console.log (i + '10 KELIPATAN ' + 10)
-
-} else {
-
-  console.log(i + " - ")
+  
+ if (i % 10 === 0){
+   
+   console.log (i + ' 10 KELIPATAN ' + 10)
+   
+ } else {
+   
+   console.log(i + " - ")
+ }
+ 
 }
