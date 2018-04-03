3231b600e014134406dc10794d67ecf9c515392b
fadhilahdirayati <fadhilahdirayati@gmail.com>
2018-04-03 10:09:59 +0700

    Update exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -1,19 +1,23 @@
 var tanggal = 20; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
 var bulan = 6;// assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = 1996; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var bulanResult;
 
-switch(bulan) {
-  case 1: { console.log(tanggal + ' Januari ' + tahun); break; }
-  case 2: { console.log(tanggal + ' Februari ' + tahun); break; }
-  case 3: { console.log(tanggal + ' Maret ' + tahun); break; }
-  case 4: { console.log(tanggal + ' April ' + tahun); break; }
-  case 5: { console.log(tanggal + ' Mei ' + tahun); break; }
-  case 6: { console.log(tanggal + ' Juni ' + tahun); break; }  
-  case 7: { console.log(tanggal + ' Juli ' + tahun); break; }
-  case 8: { console.log(tanggal + ' Agustus ' + tahun); break; }
-  case 9: { console.log(tanggal + ' September ' + tahun); break; }
-  case 10: { console.log(tanggal + ' Oktober ' + tahun); break; }
-  case 11: { console.log(tanggal + ' November ' + tahun); break; }
-  case 12: { console.log(tanggal + ' Desember ' + tahun); break; }
-  default : { console.log('Bulan tidak tersedia'); }
+switch (bulan) {
+  case 1: { bulanResult = 'Januari'; break;}
+  case 2: { bulanResult = 'Februari'; break;}
+  case 3: { bulanResult = 'Maret'; break;}
+  case 4: { bulanResult = 'April'; break;}
+  case 5: { bulanResult = 'Mei'; break;}
+  case 6: { bulanResult = 'Juni'; break;}
+  case 7: { bulanResult = 'Juli'; break;}
+  case 8: { bulanResult = 'Agustus'; break;}
+  case 9: { bulanResult = 'September'; break;}
+  case 10: { bulanResult = 'Oktober'; break;}
+  case 11: { bulanResult = 'November'; break;}
+  case 12: { bulanResult = 'Desember'; break;}
 }
+
+console.log(tanggal+' '+bulanResult+' '+tahun);
+
+
